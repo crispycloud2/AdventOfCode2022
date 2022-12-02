9c7cf83b604eb9418637327367ceb2fba3b66737
crispycloud2 <76485562+crispycloud2@users.noreply.github.com>
2022-12-02 15:41:48 +0100

    Optimized to hell

--- Day2/Source.c
+++ Day2/Source.c
@@ -3,98 +3,34 @@
 #include <string.h>
 #include <stdlib.h>
 #define IN "Input.txt"
+#define pointaddup(in) ((in) == 'A') ? 1 : ((in) == 'B') ? 2 : ((in) == 'C') ? 3 : 0
+#define rps(in1, in2) ((in1) == (in2)) ? 3 : ((in1) > (in2)) ? 0 : ((in1) < (in2)) ? 6 : 0
+#define rpsedge(in1, in2) ((in1) == 'C' && (in2) == 'A') ? 6 : ((in1) == 'A' && (in2) == 'C') ? -6 : 0
+#define winlose(in1, in2) ((in2) == 'A') ? lose(in1) : ((in2) == 'B') ? tie(in1) : ((in2) == 'C') ? win(in1) : 0
+#define lose(in) ((in) == 'A') ? 3 : ((in) == 'B') ? 1 : ((in) == 'C') ? 2 : 0
+#define tie(in) ((in) == 'A') ? 1+3 : ((in) == 'B') ? 2+3 : ((in) == 'C') ? 3+3 : 0
+#define win(in) ((in) == 'A') ? 2+6 : ((in) == 'B') ? 3+6 : ((in) == 'C') ? 1+6 : 0
 int main(void) {
 	FILE* fp = fopen(IN, "r");
 	char elf1;
 	char elf2;
 	char throwaway;
-	int result = 0;
+	int result1 = 0;
+	int result2 = 0;
 	if (fp == NULL){
 		printf("file doesnt exist");
 		exit(1);
 	}
-	while ((elf1 = getc(fp)) != EOF) { //part one
-		throwaway = getc(fp);
-		elf2 = getc(fp);
-		result = pointaddup(result, elf2);
-		throwaway = getc(fp);
-		result = rockpaperscissors(result, elf1, elf2);
-	}
-	printf("answer part 1 %d\n", result);
-	result = 0;
-	fclose(fp);
-	fp = fopen(IN, "r");
 	while ((elf1 = getc(fp)) != EOF) {
 		throwaway = getc(fp);
-		elf2 = getc(fp);
-		result = part2(result, elf1, elf2);
+		elf2 = getc(fp) - 23;
+		result1 += pointaddup(elf2);
 		throwaway = getc(fp);
+		result1 += (rps(elf1, elf2)) + (rpsedge(elf1, elf2));
+		result2 += winlose(elf1, elf2);
 	}
-	printf("answer part 2 %d\n", result);
+	printf("answer part 1 %d\n", result1);
+	printf("answer part 2 %d\n", result2);
+	fclose(fp);
 	return 0;
-}
-
-int pointaddup(int result, char ch){
-	switch (ch) {
-		case 'X'://rock
-			result += + 1;
-			break;
-		case 'Y'://paper
-			result += + 2;
-			break;
-		case 'Z'://scissors
-			result += + 3;
-			break;
-	}
-	return result;
-}
-int rockpaperscissors(int result, char elf1, char elf2) {
-	if (elf1 == 'A' && elf2 == 'X' || elf1 == 'B' && elf2 == 'Y' || elf1 == 'C' && elf2 == 'Z'){//tie
-		result += 3;
-	}
-	else if (elf1 == 'A' && elf2 == 'Y' || elf1 == 'B' && elf2 == 'Z' || elf1 == 'C' && elf2 == 'X'){//win
-		result += 6;
-	}
-	else if (elf1 == 'A' && elf2 == 'Z' || elf1 == 'B' && elf2 == 'X' || elf1 == 'C' && elf2 == 'Y'){//loss
-		result += 0;
-	}
-	return result;
-}
-int part2(int result, char elf1, char elf2) {
-	switch (elf2) {
-	case 'X'://loss
-		if (elf1 == 'A') {
-			result += 3;
-		}
-		else if (elf1 == 'B') {
-			result += 1;
-		}
-		else{
-			result += 2;
-		}
-		break;
-	case 'Y'://tie
-		if (elf1 == 'A') {
-			result += 3 + 1;
-		}
-		else if (elf1 == 'B') {
-			result += 3 + 2;
-		}
-		else {
-			result += 3 + 3;
-		}
-		break;
-	case 'Z'://win
-		if (elf1 == 'A') {
-			result += 6 + 2;
-		}
-		else if (elf1 == 'B') {
-			result += 6 + 3;
-		}
-		else {
-			result += 6 + 1;
-		}
-		break;
-	}
-	return result;
 }
